c6d621337fa4aed784a00fd84a7508c7f5d17ba7
jusaaatin <justin.dt@icloud.com>
2024-07-04 22:46:18 +0800

    you can now copy socials by pressing on it yay (haptics pending)

--- Locket/ProfileView/ProfileViewSocials.swift
+++ Locket/ProfileView/ProfileViewSocials.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct ProfileViewSocials: View {
     
@@ -55,41 +56,106 @@ struct ProfileViewSocials: View {
         }
     }
     
+    @State var socialsCounter = -1
+    @State var socialPlatform: [socialPlatforms] = []
+    @State var rawStringPRE: [String] = []
+    @State var rawStringMAIN: [String] = []
+    @State var appendedStringPRE: [String] = []
+    @State var appendedStringMAIN: [String] = []
+    @State var copying: [Bool] = []
+    @State var justAppeared = true
+    
+    private let pasteboard = UIPasteboard.general
+    
+    func loadAllSocials() {
+        for social in socials {
+            socialsCounter += 1
+            copying.append(false)
+            socialPlatform.append(social.socialPlatform)
+            rawStringPRE.append(social.stringPRE)
+            rawStringMAIN.append(social.stringMAIN)
+            appendedStringPRE.append("+\(social.stringPRE) ")
+            appendedStringMAIN.append("@\(social.stringMAIN)")
+        }
+    }
+    
+    func copySocials(count: Int) {
+        let iRawPRE = rawStringPRE[count]
+        let iRawMAIN = rawStringMAIN[count]
+        let iAppendedPRE = appendedStringPRE[count]
+        let iAppendedMAIN = appendedStringMAIN[count]
+        
+        copying[count] = true
+        
+        if socialPlatform[count] == .PhoneNumber {
+            appendedStringPRE[count] = "Copied!"
+            rawStringMAIN[count] = ""
+            pasteboard.string = "+\(iRawPRE) \(iRawMAIN)"
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+                appendedStringPRE[count] = iAppendedPRE
+                rawStringMAIN[count] = iRawMAIN
+                copying[count] = false
+            }
+        } else {
+            appendedStringMAIN[count] = "Copied!"
+            pasteboard.string = "\(iRawMAIN)"
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+                appendedStringMAIN[count] = iAppendedMAIN
+                copying[count] = false
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            if socials.count > 0 {
-                ForEach(socials) { social in
+            if socialsCounter > -1 {
+                ForEach(0 ... socialsCounter, id: \.self) { count in
                     HStack {
-                        if social.socialPlatform == .Instagram {
-                            Image(social.socialPlatform.rawValue.lowercased())
-                                .font(.system(size: 18, weight: .semibold, design: .rounded))
-                                .foregroundStyle(LinearGradient(colors: [.purple, .red,  .yellow], startPoint: .topTrailing, endPoint: .bottomLeading))
-                        } else if social.socialPlatform == .PhoneNumber {
-                            Image(social.socialPlatform.rawValue.lowercased())
-                                .font(.system(size: 18, weight: .semibold, design: .rounded))
-                                .foregroundStyle(Color.green)
-                        } else {
-                            Image(social.socialPlatform.rawValue.lowercased())
-                                .font(.system(size: 18, weight: .semibold, design: .rounded))
-                                .symbolRenderingMode(.multicolor)
-                        }
-                        Text("\(social.socialPlatform.rawValue): ")
-                            .font(.system(size: 18, weight: .semibold, design: .rounded))
                         Group {
-                            if social.socialPlatform == .PhoneNumber {
-                                Text("+\(social.stringPRE) ")
-                                    .font(.system(size: 18, weight: .regular, design: .rounded))
-                                +
-                                Text("\(social.stringMAIN)")
-                                    .font(.system(size: 18, weight: .regular, design: .rounded))
+                            if socialPlatform[count] == .Instagram {
+                                Image(socialPlatform[count].rawValue.lowercased())
+                                    .font(.system(size: 18, weight: .semibold, design: .rounded))
+                                    .foregroundStyle(LinearGradient(colors: [.purple, .red,  .yellow], startPoint: .topTrailing, endPoint: .bottomLeading))
+                            } else if socialPlatform[count] == .PhoneNumber {
+                                Image(socialPlatform[count].rawValue.lowercased())
+                                    .font(.system(size: 18, weight: .semibold, design: .rounded))
+                                    .foregroundStyle(Color.green)
                             } else {
-                                Text("@\(social.stringMAIN)")
-                                    .font(.system(size: 18, weight: .regular, design: .rounded))
+                                Image(socialPlatform[count].rawValue.lowercased())
+                                    .font(.system(size: 18, weight: .semibold, design: .rounded))
+                                    .symbolRenderingMode(.multicolor)
                             }
-                        }.minimumScaleFactor(0.1).lineLimit(1)
+                            Text("\(socialPlatform[count].rawValue): ")
+                                .font(.system(size: 18, weight: .semibold, design: .rounded))
+                            Group {
+                                if socialPlatform[count] == .PhoneNumber {
+                                    Text(appendedStringPRE[count])
+                                    +
+                                    Text(rawStringMAIN[count])
+                                } else {
+                                    Text(appendedStringMAIN[count])
+                                }
+                            }
+                            .font(.system(size: 18, weight: .regular, design: .rounded))
+                            .minimumScaleFactor(0.1)
+                            .lineLimit(1)
+                            .contentTransition(.numericText())
+                            .foregroundStyle(copying[count] ? .blue.mix(with: .white, by: 0.1) : Color("Foreground-match"))
+                            .transaction { t in
+                                if justAppeared == false {
+                                    t.animation = .default
+                                }
+                            }
+                        }.onTapGesture {
+                            if copying[count] == false && justAppeared == false{
+                                copySocials(count: count)
+                            }
+                        }
                         Spacer()
                         Button(action: {
-                            if let url = URL(string: returnSocialWebsite(platform: social.socialPlatform, pre: social.stringPRE, main: social.stringMAIN)) {
+                            if let url = URL(string: returnSocialWebsite(platform: socialPlatform[count], pre: rawStringPRE[count], main: rawStringMAIN[count])) {
                                 UIApplication.shared.open(url)
                             }
                         }, label: {
@@ -100,7 +166,7 @@ struct ProfileViewSocials: View {
                         .bold()
                         .frame(height: 4)
                     }.padding([.top, .bottom], 5)
-                    if !(socials.last == social) {
+                    if !(socialsCounter == count) {
                         Divider()
                     }
                 }
@@ -115,6 +181,12 @@ struct ProfileViewSocials: View {
                 }.frame(height: 20)
             }
         }
+        .onAppear {
+            loadAllSocials()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                justAppeared = false
+            }
+        }
         .padding()
         .background(Color.gray.mix(with:Color("Background-match"), by: 0.7))
         .clipShape(RoundedRectangle(cornerRadius: 20))
@@ -123,12 +195,12 @@ struct ProfileViewSocials: View {
 
 #Preview {
     ProfileViewSocials(socials: 
-                        [/*
+                        [
                         socials(socialPlatform: .PhoneNumber, stringPRE: "65", stringMAIN: "91234567"),
                          socials(socialPlatform: .Instagram, stringPRE: "", stringMAIN: "Username"),
                         socials(socialPlatform: .Discord, stringPRE: "", stringMAIN: "Username"),
                         socials(socialPlatform: .Slack, stringPRE: "", stringMAIN: "Username"),
                         socials(socialPlatform: .Telegram, stringPRE: "", stringMAIN: "Username"),
                         socials(socialPlatform: .Youtube, stringPRE: "", stringMAIN: "Username")
-                      */  ], demo: true)
+                        ], demo: true)
 }
